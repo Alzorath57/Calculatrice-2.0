f53c38ac985652aa6bf274a85d11aba93233e176
Alzorath57 <julien.holtzinger@gmail.com>
2026-04-16 15:52:05 +0200

    function egal

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ boutons.addEventListener("click", function (event) {
   if (boutonSelectionner.dataset.chiffre) {
     gererChiffre(boutonSelectionner.dataset.chiffre);
   }
+  if (["+", "-", "*", "/"].includes(boutonSelectionner.dataset.action)) {
+    gererOperateur(boutonSelectionner.dataset.action);
+  }
+  if (["="].includes(boutonSelectionner.dataset.action)) {
+    gererEgal(boutonSelectionner.dataset.action);
 });
 
 // affiche le premier chiffre
@@ -33,7 +38,34 @@ function gererOperateur(operateur) {
     state.operation = "";
   }
   state.operation += operateur;
-  state.calcul = state.affiche + operateur;
+  state.calcul = state.affiche + " " + operateur;
   state.affiche = "0";
   calcul.textContent = state.operation;
+}
+
+// égal
+function gererEgal(egal) {
+  const nombre1 = parseFloat(state.calcul); // premier chiffre
+  const nombre2 = parseFloat(state.affiche); // deuxième chiffre
+  let resultatFinal = 0;
+
+  if (state.operation === "+") {
+    resultatFinal = nombre1 + nombre2;
+  }
+  if (state.operation === "-") {
+    resultatFinal = nombre1 - nombre2;
+  }
+  if (state.operation === "*") {
+    resultatFinal = nombre1 * nombre2;
+  }
+  if (state.operation === "/") {
+    if (nombre2 === 0){
+        resultat.textContent = "Désolé... Nous ne pouvons pas diviser par zéro"
+        return
+    }else {
+        resultatFinal = nombre1 / nombre2;}
+    
+  }
+
+  resultat.textContent = resultatFinal;
 }
