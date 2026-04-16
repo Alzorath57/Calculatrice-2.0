72a3895483ba10b02e356c79e3afd8b9c8a29ca5
Alzorath57 <julien.holtzinger@gmail.com>
2026-04-16 15:23:06 +0200

    gerer les opérateurs

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-const calcul = document.getElementById("ligne-calcul")  //ecran 
-const resultat = document.getElementById("ligne-resultat") //ecran
-const boutons = document.querySelector(".boutons") //boutons
+const calcul = document.getElementById("ligne-calcul"); //ecran
+const resultat = document.getElementById("ligne-resultat"); //ecran
+const boutons = document.querySelector(".boutons"); //boutons
 
 //valeur initial
-const state = { 
-    affiche: '0',
-    operation: null,
-    calcul: "",
-    egal: false
-}
+const state = {
+  affiche: "0",
+  operation: null,
+  calcul: "",
+  egal: false,
+};
 
 //bouton cliqué
-boutons.addEventListener("click", function(event) {
-   
-   const chiffreSelectionne = event.target 
-    if (chiffreSelectionne.dataset.chiffre){
-        if (state.affiche === "0"){
-            state.affiche = ""
-        }
-        state.affiche += chiffreSelectionne.dataset.chiffre
-        
-        resultat.textContent = state.affiche
-    }  
+boutons.addEventListener("click", function (event) {
+  const boutonSelectionner = event.target;
+  if (boutonSelectionner.dataset.chiffre) {
+    gererChiffre(boutonSelectionner.dataset.chiffre);
+  }
+});
+
+// affiche le premier chiffre
+function gererChiffre(chiffre) {
+  if (state.affiche === "0") {
+    state.affiche = "";
+  }
+  state.affiche += chiffre;
+  resultat.textContent = state.affiche;
+}
+
+// ajoute l'opérateur
+function gererOperateur(operateur) {
+  if (state.operation === null) {
+    state.operation = "";
+  }
+  state.operation += operateur;
+  state.calcul = state.affiche + operateur;
+  state.affiche = "0";
+  calcul.textContent = state.operation;
 }
